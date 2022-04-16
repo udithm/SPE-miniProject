804e3c475aa72ba1248c900f5151922d04c09576
udithm <45118519+udithm@users.noreply.github.com>
2022-04-17 03:06:50 +0530

    Added Logging code in log4j2.xml and Calculator.java

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,22 +1,33 @@
 package calculator;
 import java.lang.Math;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.InputMismatchException;
 
 public class Calculator {
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
     public double square_root(int x){
+        logger.info("Square root of "+x+"\t"+ Math.sqrt(x));
         return Math.sqrt(x);
     }
 
     public double factorial(int x){
-        if (x == 0)
-            return 1;
-        return x*factorial(x-1);
+        double res = 1;
+        for(int i =2;i<=x;i++)
+        {
+            res = res*i;
+        }
+        logger.info("Factorial of "+x+"\t"+ res);
+        return res;
     }
 
     public double log(int x){
+        logger.info("Logarithm of "+x+"\t"+ Math.log(x));
         return Math.log(x);
     }
 
     public double power(int x, int y){
+        logger.info("Power of " + x +"raised to" + y + " is: " + Math.pow(x, y));
         return Math.pow(x,y);
     }
 }
