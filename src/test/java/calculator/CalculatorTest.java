a2c2861c6a35b70d76625c7165eb2b0acb7b0a14
udithm <45118519+udithm@users.noreply.github.com>
2022-04-18 15:18:35 +0530

    Updated Code
    Changed Test Class

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -7,58 +7,58 @@ public class CalculatorTest {
     Calculator calc = new Calculator();
 
     @Test
-    public void squarerootTruePositive()
+    public void squareroot_TruePositive()
     {
-        assertEquals ("Square root of an Int - True Positive", 2, calc.square_root(4), DELTA);
-        assertEquals ("Square root of an Int - True Positive", 4, calc.square_root(16), DELTA);
+        assertEquals ("True Positive - Square root of an Int ", 2, calc.square_root(4), DELTA);
+        assertEquals ("True Positive - Square root of an Int ", 4, calc.square_root(16), DELTA);
     }
 
     @Test
-    public void squarerootFalsePositive()
+    public void squareroot_FalsePositive()
     {
-        assertNotEquals ("Square root of an Int - True Positive", 3, calc.square_root(7), DELTA);
-        assertNotEquals ("Square root of an Int - True Positive", 5, calc.square_root(49), DELTA);
+        assertNotEquals ("False Positive - Square root of an Int ", 3, calc.square_root(7), DELTA);
+        assertNotEquals ("False Positive - Square root of an Int ", 5, calc.square_root(49), DELTA);
     }
 
     @Test
-    public void factorialTruePositive()
+    public void factorial_TruePositive()
     {
-        assertEquals ("Square root of an Int - True Positive", 1, calc.factorial(1), DELTA);
-        assertEquals ("Square root of an Int - True Positive", 120, calc.factorial(5), DELTA);
+        assertEquals ("True Positive - factorial of an Int", 1, calc.factorial(1), DELTA);
+        assertEquals ("True Positive - factorial of an Int", 120, calc.factorial(5), DELTA);
     }
 
     @Test
-    public void factorialFalsePositive()
+    public void factorial_FalsePositive()
     {
-        assertNotEquals ("Square root of an Int - True Positive", 0, calc.factorial(0), DELTA);
-        assertNotEquals ("Square root of an Int - True Positive", 12, calc.factorial(4), DELTA);
+        assertNotEquals ("False Positive - factorial of an Int", 0, calc.factorial(0), DELTA);
+        assertNotEquals ("False Positive - factorial of an Int", 12, calc.factorial(4), DELTA);
     }
 
     @Test
-    public void logTruePositive()
+    public void log_TruePositive()
     {
-        assertEquals ("Square root of an Int - True Positive", 0, calc.log(1), DELTA);
-        assertEquals ("Square root of an Int - True Positive", 2.302585092994046, calc.log(10), DELTA);
+        assertEquals ("True Positive - logarithm of an Int", 0, calc.log(1), DELTA);
+        assertEquals ("True Positive - logarithm of an Int", 2.302585092994046, calc.log(10), DELTA);
     }
 
     @Test
-    public void logFalsePositive()
+    public void log_FalsePositive()
     {
-        assertNotEquals ("Square root of an Int - True Positive", 0, calc.log(2), DELTA);
-        assertNotEquals ("Square root of an Int - True Positive", 12, calc.log(4), DELTA);
+        assertNotEquals ("False Positive - logarithm of an Int", 0, calc.log(2), DELTA);
+        assertNotEquals ("False Positive - logarithm of an Int", 12, calc.log(4), DELTA);
     }
 
     @Test
-    public void powerTruePositive()
+    public void power_TruePositive()
     {
-        assertEquals ("Square root of an Int - True Positive", 0, calc.power(0, 1), DELTA);
-        assertEquals ("Square root of an Int - True Positive", 100, calc.power(10, 2), DELTA);
+        assertEquals ("True Positive - power of base Int to power Int ", 0, calc.power(0, 1), DELTA);
+        assertEquals ("True Positive - power of base Int to power Int ", 100, calc.power(10, 2), DELTA);
     }
 
     @Test
-    public void powerFalsePositive()
+    public void power_FalsePositive()
     {
-        assertNotEquals ("Square root of an Int - True Positive", 0, calc.power(2, 0), DELTA);
-        assertNotEquals ("Square root of an Int - True Positive", 12, calc.power(4, 3), DELTA);
+        assertNotEquals ("False Positive - power of base Int to power Int ", 0, calc.power(2, 0), DELTA);
+        assertNotEquals ("False Positive - power of base Int to power Int ", 12, calc.power(4, 3), DELTA);
     }
 }
